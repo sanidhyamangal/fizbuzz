e3587ac1490b35dfa739c0f1833cfbd9f7912f1a
Sanidhya Mangal <sanidhyamangal@Sanidhyas-MacBook-Pro.local>
2025-02-20 21:00:08 -0600

    feat: yahoo finance tool
    added yahoo finance tool specs and an example notebook for using the spec tools

--- src/fizbuzz/tools/yahoo_finance.py
+++ src/fizbuzz/tools/yahoo_finance.py
@@ -2,9 +2,11 @@
 author: Sanidhya Mangal
 email: mangalsanidhya19@gmail.com
 """
-from llama_index.core.tools.tool_spec.base import BaseToolSpec
+
 import pandas as pd
 import yfinance as yf
+from llama_index.core.tools.tool_spec.base import BaseToolSpec
+
 
 class YahooFinanceToolSpec(BaseToolSpec):
     """A Tool to pull all the financial data from yahoo finance
@@ -16,15 +18,14 @@ class YahooFinanceToolSpec(BaseToolSpec):
         "cash_flow",
         "income_statement",
         "basic_stock_information",
-        "analyst_recommendations"
-        "stock_news",
-        "run_spec_function_for_multiple_tickers"
+        "analyst_recommendations" "stock_news",
+        "run_spec_function_for_multiple_tickers",
     ]
 
     def __init__(self):
         """Initialize the Yahoo Finance tool spec."""
-    
-    def balance_sheet(self, ticker:str) -> str:
+
+    def balance_sheet(self, ticker: str) -> str:
         """A function to download balance sheet from yahoo finance for a given ticker
 
         Args:
@@ -37,7 +38,7 @@ class YahooFinanceToolSpec(BaseToolSpec):
         stock = yf.Ticker(ticker=ticker)
         balance_sheet = pd.DataFrame(stock.balance_sheet)
         return f"Balance Sheet:\n{balance_sheet.to_string()}"
-    
+
     def cash_flow(self, ticker: str) -> str:
         """A function to return chashflow of a stock from yahoo finance
 
@@ -51,8 +52,8 @@ class YahooFinanceToolSpec(BaseToolSpec):
         stock = yf.Ticker(ticker=ticker)
         cash_flow = pd.DataFrame(stock.cash_flow)
         return f"Cash Flow:\n{cash_flow.to_string()}"
-    
-    def basic_stock_information(self, ticker:str) -> str:
+
+    def basic_stock_information(self, ticker: str) -> str:
         """A function to return basic stock information. Eg. price, description, name
 
         Args:
@@ -64,7 +65,7 @@ class YahooFinanceToolSpec(BaseToolSpec):
 
         stock = yf.Ticker(ticker=ticker)
         return f"Basic Stock information:\n{stock.info}"
-    
+
     def analyst_recommendations(self, ticker: str) -> str:
         """Get the analyst recommendations for the stocks
 
@@ -76,7 +77,7 @@ class YahooFinanceToolSpec(BaseToolSpec):
         """
         stock = yf.Ticker(ticker)
         return f"Recommendations:\n{stock.recommendations}"
-    
+
     def stock_news(self, ticker: str) -> str:
         """Get most recent news titles for the stock
 
@@ -90,7 +91,7 @@ class YahooFinanceToolSpec(BaseToolSpec):
         stock_news = stock.news
         titles = "\n".join(news["content"]["title"] for news in stock_news)
         return f"News:\n{titles}"
-    
+
     def income_statement(self, ticker: str) -> str:
         """Get income statement for the stock
 
@@ -104,8 +105,10 @@ class YahooFinanceToolSpec(BaseToolSpec):
         stock = yf.Ticker(ticker)
         income_statement = pd.DataFrame(stock.income_stmt)
         return f"Income Statement:\n{income_statement.to_string()}"
-    
-    def run_spec_function_for_multiple_tickers(self, tickers: list[str], method:str) -> dict[str, str]:
+
+    def run_spec_function_for_multiple_tickers(
+        self, tickers: list[str], method: str
+    ) -> dict[str, str]:
         """Run a spec_function method for the stocks
 
         Args:
@@ -120,5 +123,5 @@ class YahooFinanceToolSpec(BaseToolSpec):
             if not hasattr(self, method):
                 return_dict[ticker] = "Invalid method, no data found."
             return_dict[ticker] = getattr(self, method)(ticker)
-        
+
         return return_dict
